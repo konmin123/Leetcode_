0bc9fdd82d356aa3c8f022dd758a46f4f41bb8cf
konmin <MindlinKS@yandex.ru>
2022-07-16 01:34:27 +0300

    refactor

---
+++ leetcode6.py
@@ -0,0 +1,46 @@
+"""21. Merge Two Sorted Lists
+Easy
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+
+
+Constraints:
+
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+
+"""
+from typing import List
+
+list_1 = [1, 1, 3]
+list_2 = [2, 2, 5]
+
+
+class Merger:
+    @staticmethod
+    def merge_two_sorted_lists(first_list: List[int], second_list: List[int]) -> List[int]:
+        for _ in range(len(list_1)+len(list_2)):
+            pass
