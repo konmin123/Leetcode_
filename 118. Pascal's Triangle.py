bf0719954245d880cdefdbbc2b9f2ff9af4c9ca6
konmin <MindlinKS@yandex.ru>
2022-07-30 14:39:28 +0300

    complete

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -24,17 +24,19 @@ class Solution:
         for i in range(1, num_rows + 1):
             if i == 1:
                 output.append([1])
+                continue
             if i == 2:
                 output.append([1, 1])
+                continue
             else:
-                list_for_add = []
-                for j in range(len(output[-1])-1):
-                    ...
-
-
+                list_for_add = [1, 1]
+                for j in range(1, len(output[-1])):
+                    x = output[-1][j-1] + output[-1][j]
+                    list_for_add.insert(j, x)
+                output.append(list_for_add)
         return output
 
 
 if __name__ == '__main__':
-    print(Solution.generate(5))
+    print(Solution.generate(10))
 
