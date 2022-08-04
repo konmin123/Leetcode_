b4e54a37e9d59125bbaecdf7267e91210ee3f3d2
konmin <MindlinKS@yandex.ru>
2022-08-04 15:43:32 +0300

    complete

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -35,4 +35,25 @@ Constraints:
 class Solution:
     @staticmethod
     def convert_to_title(column_number: int) -> str:
-        ...
+        alphabet = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L",
+                    13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W",
+                    24: "X", 25: "Y", 26: "Z"}
+        column_number_for_while = column_number
+        i = 1
+        while column_number_for_while > 0:
+            if column_number_for_while - 26 ** i > 0:
+                column_number_for_while = column_number_for_while - 26 ** i
+                i += 1
+            else:
+                break
+        num = column_number
+        output = ""
+        for i_ in range(i-1, 0, -1):
+            output += (alphabet[num // 26 ** i_])
+            num = num - (26 ** i_) * (num // 26 ** i_)
+        output += (alphabet[num])
+        return str(output)
+
+
+if __name__ == '__main__':
+    print(Solution.convert_to_title(16381))
