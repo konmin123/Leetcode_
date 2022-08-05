1e14e2f8a0ef37d5b2f799f2019f7a58993c4667
konmin <MindlinKS@yandex.ru>
2022-08-05 11:29:41 +0300

    init

---
+++ 203. Remove Linked List Elements.py
@@ -0,0 +1,44 @@
+"""Given the head of a linked list and an integer val,
+remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Example 1:
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 104].
+    1 <= Node.val <= 50
+    0 <= val <= 50
+
+"""
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+node1 = ListNode(1, node2)
+
+
+class Solution:
+    def remove_elements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        ...
