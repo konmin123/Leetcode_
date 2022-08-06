9c49464b512a761bb35633eff645ba68c6836151
konmin <MindlinKS@yandex.ru>
2022-08-06 12:49:19 +0300

    complete O(n), memory O(1)

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -36,12 +36,65 @@ from typing import Optional
 
 
 class ListNode:
-    def __init__(self, x, next_=None):
-        self.val = x
+    def __init__(self, val, next_=None):
+        self.val = val
         self.next = next_
+
+    @staticmethod
+    def init_linked_list(seq):
+        prev = None
+        for value in seq[::-1]:
+            if not prev:
+                prev = ListNode(val=value)
+            else:
+                prev = ListNode(val=value, next_=prev)
+        return prev
+
+    @staticmethod
+    def init_cycle_for_linked_list(head, pos: int):
+        current_node = head
+        pos_node = None
+        count = 1
+        while True:
+            if current_node.next is None:
+                current_node.next = pos_node
+                break
+            if count == pos:
+                pos_node = current_node
+            current_node = current_node.next
+            count +=1
+        return head
+
+    @staticmethod
+    def print_value_linked_list(head):
+        count = 1
+        current_node = head
+        while count < 15:
+            print(current_node.val)
+            current_node = current_node.next
+            count += 1
+            if current_node is None:
+                break
 
 
 class Solution:
+
     @staticmethod
     def has_cycle(head: Optional[ListNode]) -> bool:
-        ...
+        temp1 = head
+        temp2 = head
+        while temp1.next and temp2.next and temp2.next.next:
+            temp1 = temp1.next
+            temp2 = temp2.next.next
+            if temp1 == temp2:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    z = ListNode.init_linked_list([1, 2])
+    print(z)
+    ListNode.init_cycle_for_linked_list(z, 1)
+    ListNode.print_value_linked_list(z)
+    print(Solution.has_cycle(z))
+
