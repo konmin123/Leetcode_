54aa9173b2ea4c57ba51d9f1e51d731b9faa5c55
konmin <MindlinKS@yandex.ru>
2022-09-08 12:03:26 +0300

    O(n**2) inplace

---
+++ bubble sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(seq: list) -> list:
+    for position in range(len(seq) - 1):
+        for index in range(len(seq) - position - 1):
+            if seq[index] > seq[index + 1]:
+                seq[index], seq[index + 1] = seq[index + 1], seq[index]
+                print(seq)
+    return seq
+
+
+if __name__ == '__main__':
+    print(bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
