1b965ca10755b24e6c8f17b911bc284770db369d
konmin <MindlinKS@yandex.ru>
2022-09-24 01:45:59 +0300

    add decision time complexity O(N), memory O(N)

--- days_temperature.py
+++ days_temperature.py
@@ -1,6 +1,7 @@
 """You are given a list of daytime temperatures, return a list of the number of days to a warmer day.
 example of input data: [15, 11, 10, 12, 9, 16, 17]
 output: [5, 2, 1, 2, 1, 'n/a']"""
+from collections import deque
 
 seq = [15, 11, 10, 12, 9, 16, 17]
 
@@ -19,6 +20,27 @@ def brutal_force(sequence: list) -> list:
     return output
 
 
-print(brutal_force(seq))
+def decision(sequence: list) -> list:
+    output = deque()
+    stack = deque()
+    index = len(sequence) - 1
+    for temp in sequence[::-1]:
+        if not stack:
+            stack.append((temp, index))
+            output.append('n/a')
+        else:
+            if stack[-1][0] > temp:
+                output.appendleft(stack[-1][1] - index)
+                stack.append((temp, index))
+            else:
+                while temp > stack[-1][0]:
+                    stack.pop()
+                output.appendleft(stack[-1][1] - index)
+                stack.append((temp, index))
+        index -= 1
+    return list(output)
+
+
+print(decision(seq))
 
 
