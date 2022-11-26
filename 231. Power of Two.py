45defeef0d04e59a158ec8dc0773f82e460c0b44
konmin <MindlinKS@yandex.ru>
2022-11-26 13:48:48 +0300

    add binary_search_solution and math_log_solution

--- 231. Power of Two.py
+++ 231. Power of Two.py
@@ -1,6 +1,7 @@
 """Given an integer n, return true if it is a power of two. Otherwise, return false.
 
 An integer n is a power of two, if there exists an integer x such that n == 2x."""
+import math
 
 
 class Solution:
@@ -25,7 +26,29 @@ class Solution:
             return True
         return False
 
+    @staticmethod
+    def binary_search_solution(n: int) -> bool:
+        left_border = 0
+        right_border = math.log(n, 2)
+        while left_border <= right_border:
+            middle = int((left_border + right_border) / 2)
+            if 2 ** middle == n:
+                return True
+            elif 2 ** middle < n:
+                left_border = middle + 1
+            else:
+                right_border = middle + 1
+        return False
+
+    @staticmethod
+    def math_log_solution(n: int) -> bool:
+        float_ = math.log(n, 2)
+        int_ = int(float_)
+        if int_ == float_:
+            return True
+        return False
+
 
 if __name__ == '__main__':
-    print(Solution.binary_solution(257))
+    print(Solution.math_log_solution(1028))
 
