1d5000dff76122eb9baf353043292580868658bd
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2022-12-22 13:32:39 +0300

    complete

---
+++ 415. Add Strings.py
@@ -0,0 +1,32 @@
+"""Given two non-negative integers, num1 and num2 represented as string, return
+the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling
+large integers (such as BigInteger). You must also not convert the inputs to
+integers directly.
+
+Example 1:
+Input: num1 = "11", num2 = "123"
+Output: "134"
+
+Example 2:
+Input: num1 = "456", num2 = "77"
+Output: "533"
+
+Example 3:
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+Constraints:
+1 <= num1.length, num2.length <= 104
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself."""
+
+
+class Solution:
+    @staticmethod
+    def add_strings(num1: str, num2: str) -> str:
+        return str(eval(num1) + eval(num2))
+
+
+print(Solution.add_strings('123', '123'))
