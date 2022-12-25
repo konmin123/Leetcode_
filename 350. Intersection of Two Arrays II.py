d7a864b77f9069ccb927ca28cce891918d0a3a56
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2022-12-25 18:24:49 +0300

    complete, complexity O(n1) - min/max style

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,42 @@
+"""Given two integer arrays nums1 and nums2, return an array of their
+intersection. Each element in the result must appear as many times as it shows
+in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+
+Constraints:
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your
+algorithm? What if nums1's size is small compared to nums2's size?
+Which algorithm is better? What if elements of nums2 are stored on disk,
+and the memory is limited such that you cannot load all elements into the
+memory at once?"""
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+        output: List[int] = []
+        for element in min(nums1, nums2):
+            if element in max(nums1, nums2):
+                output.append(element)
+        return output
+
+
+assert Solution.intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
+assert Solution.intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [4, 9]
+assert Solution.intersect([2, 2], [1, 2, 2, 1]) == [2, 2]
+assert Solution.intersect([9, 4, 9, 8, 4], [4, 9, 5]) == [4, 9]
+assert Solution.intersect([], [2, 2]) == []
