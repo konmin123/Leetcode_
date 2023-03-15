157cd7caae6666af9dcb071add90006c47f10733
Константин <MindlinKS@yandex.ru>
2023-03-15 21:20:35 +0300

    refactoring

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -1,7 +1,9 @@
-"""Given head, the head of a linked list, determine if the linked list has a cycle in it.
+"""Given head, the head of a linked list, determine if the linked list has a
+cycle in it.
 
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following
-the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+There is a cycle in a linked list if there is some node in the list that can be
+reached again by continuously following the next pointer. Internally, pos is
+used to denote the index of the node that tail's next pointer is connected to.
 Note that pos is not passed as a parameter.
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
@@ -10,13 +12,15 @@ Example 1:
 
 Input: head = [3,2,0,-4], pos = 1
 Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Explanation: There is a cycle in the linked list, where the tail connects to
+the 1st node (0-indexed).
 
 Example 2:
 
 Input: head = [1,2], pos = 0
 Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Explanation: There is a cycle in the linked list, where the tail connects to
+the 0th node.
 
 Example 3:
 
@@ -62,7 +66,7 @@ class ListNode:
             if count == pos:
                 pos_node = current_node
             current_node = current_node.next
-            count +=1
+            count += 1
         return head
 
     @staticmethod
@@ -97,4 +101,3 @@ if __name__ == '__main__':
     ListNode.init_cycle_for_linked_list(z, 1)
     ListNode.print_value_linked_list(z)
     print(Solution.has_cycle(z))
-
