f09641f078490c17ab453cb05fe7afefdbb136ae
Константин <MindlinKS@yandex.ru>
2023-04-04 18:05:58 +0300

    complete, complexity O(N), memory O(N)

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -33,6 +33,3 @@ assert Solution.length_of_last_word("World") == 5
 assert Solution.length_of_last_word("Hello World") == 5
 assert Solution.length_of_last_word("   fly me   to   the moon  ") == 4
 assert Solution.length_of_last_word("luffy is still joyboy") == 6
-
-
-
