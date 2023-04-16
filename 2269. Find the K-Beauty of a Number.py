bc6010c630e3e9e61a089af5fb0e2527b7f3e8a8
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-04-16 20:48:57 +0300

    complete, complexity O(n), memory O(1)

---
+++ 2269. Find the K-Beauty of a Number.py
@@ -0,0 +1,54 @@
+"""The k-beauty of an integer num is defined as the number of substrings of num
+when it is read as a string that meet the following conditions:
+It has a length of k.
+It is a divisor of num.
+Given integers num and k, return the k-beauty of num.
+
+Note:
+Leading zeros are allowed.
+0 is not a divisor of any value.
+A substring is a contiguous sequence of characters in a string.
+
+Example 1:
+Input: num = 240, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "24" from "240": 24 is a divisor of 240.
+- "40" from "240": 40 is a divisor of 240.
+Therefore, the k-beauty is 2.
+
+Example 2:
+Input: num = 430043, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "43" from "430043": 43 is a divisor of 430043.
+- "30" from "430043": 30 is not a divisor of 430043.
+- "00" from "430043": 0 is not a divisor of 430043.
+- "04" from "430043": 4 is not a divisor of 430043.
+- "43" from "430043": 43 is a divisor of 430043.
+Therefore, the k-beauty is 2.
+
+Constraints:
+1 <= num <= 109
+1 <= k <= num.length (taking num as a string)"""
+
+
+class Solution:
+    @staticmethod
+    def divisor_substrings(num: int, k: int) -> int:
+        output: int = 0
+        str_num = str(num)
+        cur_l: int = 0
+        cur_r = k
+        while cur_r <= len(str_num):
+            number = int(str_num[cur_l: cur_r])
+            if number != 0 and num % number == 0:
+                output += 1
+            cur_l += 1
+            cur_r += 1
+        return output
+
+
+assert Solution.divisor_substrings(430043, 2) == 2
+assert Solution.divisor_substrings(240, 2) == 2
+assert Solution.divisor_substrings(2, 1) == 1
