90d952606619d48d3a474b56e22ea531e744a927
Константин <MindlinKS@yandex.ru>
2023-10-04 12:09:25 +0300

    complete, complexity O(n), memory O(1)

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -43,3 +43,4 @@ assert Solution.two_sum([2, 7, 11, 15], 9) == [0, 1]
 assert Solution.two_sum([15, 11, 2, 7], 9) == [2, 3]
 assert Solution.two_sum([3, 2, 4], 6) == [1, 2]
 assert Solution.two_sum([3, 3], 6) == [0, 1]
+
